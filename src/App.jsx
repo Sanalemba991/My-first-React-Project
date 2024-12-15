ad9639575bc10e0cd583b35feb71b78340cad537
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-15 23:20:46 +0530

    add Video

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,16 @@
-import React, { useState } from 'react';
+import React from "react";
+import Vid from "./video/Vid.mp4";
 
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [submit, setSubmit] = useState(false); // Fixed the initial state
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, password, confirmPassword);
-    setSubmit(true); 
-  };
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          placeholder="Enter your name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          value={email}
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="password"
-          value={confirmPassword}
-          placeholder="Confirm Password"
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <h1>My First React Project for Video</h1>
+    <iframe
+    height={350}
+    width={907}
+    src={Vid}
+    allowFullScreen/>
 
-     
-      {submit && (
-        <div>
-          <p>Name: {name}</p>
-          <p>Email: {email}</p>
-        </div>
-      )}
     </div>
   );
 }
