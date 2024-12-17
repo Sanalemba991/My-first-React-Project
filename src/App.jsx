65bb52b48eca33bd0e0f9a7c48a6f69f6565240f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-17 17:08:35 +0530

    back to the first

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import Vid from "./video/Vid.mp4"
+
 function App() {
   return (
     <>
     <h1>My First React Project for Video</h1>
     <div className="my">
      
-  <video controls>
-              
-              <source src={Vid}></source></video>
+      <iframe
+        className="bor"
+        height={350}
+        width={907}
+        frameBorder={0}
+        src="https://www.youtube.com/embed/pqZuSz8_2DM?list=RDCLAK5uy_ksEjgm3H_7zOJ_RHzRjN1wY-_FFcs7aAU&start=0"
+        allowFullScreen
+      />
       <img
         className="sam"
-        src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/03/is-shanks-evil-in-one-piece.jpg?q=50&fit=crop&w=1140&h=&dpr=1." alt="Shanks"
+        src="https://images2.alphacoders.com/136/1369766.jpeg"
       />
       <h1 className="ram">Shanks</h1>
       <div>
