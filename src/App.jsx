3a7f11de5f0c21e99e16d04a4f2efdfa438319c8
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-17 17:08:06 +0530

    video adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Vid from "Vid.mp4"
+import Vid from "./video/Vid.mp4"
 function App() {
   return (
     <>
     <h1>My First React Project for Video</h1>
     <div className="my">
      
-    <video src=""></video>
+    <video src={Vid}></video>
       <img
         className="sam"
         src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/03/is-shanks-evil-in-one-piece.jpg?q=50&fit=crop&w=1140&h=&dpr=1." alt="Shanks"
