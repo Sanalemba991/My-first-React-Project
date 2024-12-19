07bcf7bf9ee81469946460d350eb33f82380a29b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-19 20:11:44 +0530

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import React from "react";
+import React from 'react'
 
 function App() {
   return (
-    <>
-    <h1>My First React Project for Video</h1>
-    <div className="my">
-     
-      <iframe
-        className="bor"
-        height={350}
-        width={907}
-        frameBorder={0}
-        src="https://www.youtube.com/embed/pqZuSz8_2DM?list=RDCLAK5uy_ksEjgm3H_7zOJ_RHzRjN1wY-_FFcs7aAU&start=0"
-        allowFullScreen
-      />
-      <img
-        className="sam"
-        src="https://images2.alphacoders.com/136/1369766.jpeg"
-      />
-      <h1 className="ram">Shanks</h1>
-      <div>
-      <button className="but">Click Me</button>
-      </div>
-     
+    <div>
+      Samba
     </div>
-  
-    </>
-  );
+  )
 }
 
-export default App;
+export default App
