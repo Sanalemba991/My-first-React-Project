f24799f5c31518d255a844c6fa28f4a6546b4d8d
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-23 19:54:32 +0530

    home screen added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./App.css"
 
 function Home() {
   return (
@@ -7,7 +8,7 @@ function Home() {
       <h1>Home Page for My Project</h1>
       <p>haka-based International Crimes Tribunal (ICT) has issued arrest warrants for Hasina and several former Cabinet ministers, advisers, and military and civil officials for "crimes against humanity and genocide".</p>
    
-      <Link to="/sana">Go to Sanalemba</Link>
+      <Link className='sam' to="/sana">Go to Sanalemba</Link>
       
     </div>
   )
