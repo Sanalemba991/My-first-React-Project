ee178b1ded96d3de19a270932df68cf351e30df3
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-23 20:22:57 +0530

    Home add it

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./App.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function Home() {
   return (
     <div>
+      <div className="op">
+        <Link className="sam" to="/sana">
+          Go to Sanalemba
+        </Link>
+        <Link className="sam" to="/prenita">
+          Got to Prenita
+        </Link>
+        <Link className="sam" to="/react">
+          Go to React
+        </Link>
+      </div>
       <h1>Home Page for My Project</h1>
-      <p>haka-based International Crimes Tribunal (ICT) has issued arrest warrants for Hasina and several former Cabinet ministers, advisers, and military and civil officials for "crimes against humanity and genocide".</p>
-   
-      <Link className='sam' to="/sana">Go to Sanalemba</Link>
-      
+      <p>
+        haka-based International Crimes Tribunal (ICT) has issued arrest
+        warrants for Hasina and several former Cabinet ministers, advisers, and
+        military and civil officials for "crimes against humanity and genocide".
+      </p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
