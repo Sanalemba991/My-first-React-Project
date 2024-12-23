ce928d4863dab4982e6c3b4c063a738978979b87
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-23 20:23:20 +0530

    prenita add it

--- src/components/Prenita.jsx
+++ src/components/Prenita.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Prenita() {
   return (
     <div>
-      Prenita
+      <Link className="sam" to="/">
+        Go to Home Page
+      </Link>
+      <h1>Filmmaker Shyam Benegal, 90, passes away</h1>
+      <p>
+        A towering figure in Indian cinema and a doyen of the parallel cinema
+        movement, Benegal’s influence on the industry was unparalleled.
+      </p>
     </div>
-  )
+  );
 }
 
-export default Prenita
+export default Prenita;
