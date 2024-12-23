616cc8a4baea76faa670922a8285fcff365056a2
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-23 20:23:03 +0530

    react add it

--- src/REACT.jsx
+++ src/REACT.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function REACT() {
   return (
     <div>
-      Hi
+        <Link className='sam' to="/">Go to Home Page</Link>
+      <h1>A CM, a film star and a tragedy: A Hyderabad post-script with twists and turns</h1>
+      <p>CM Revanth Reddy's aides claim he wants to send message of "no one being above the law"; his opponents see it as a sign of his "insecurity", accuse him of "targeting the film industry".</p>
     </div>
   )
 }
