6c1eaf5e5fbef0efac012311ba17d71598898807
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-28 19:39:53 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 
-function SimpleExample() {
-  const [message, setMessage] = useState('Hello, world!');
-
-  useEffect(() => {
-    
-    const timer = setTimeout(() => {
-      setMessage('State updated after 2 seconds');
-    }, 2000);
-
-   
-  }, []); 
-
+function App() {
+  const[sana,setSana]=useState('sana')
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+      setSana('State is updates')
+    },2000)
+  },[])
   return (
     <div>
-      <h1>{message}</h1>
+      <h>{sana}</h>
     </div>
-  );
+  )
 }
 
-export default SimpleExample;
+export default App
