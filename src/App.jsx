cee2f10d451b63f3ff1393fc5330f15fcac97329
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-03 17:14:29 +0530

    sadasd

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,17 @@ function App() {
   const [sana, setSana] = useState("");
   const [result, setResult] = useState("");
 
-
   const change = (e) => {
     setSana(e.target.value);
   };
 
-  
   const Fangba = () => {
-    setResult(sana); 
+    setResult(sana);
   };
 
   return (
     <div>
-   
-      <input
-        value={sana}
-        onChange={change}
-        placeholder="Type something"
-      />
+      <input value={sana} onChange={change} placeholder="Type something" />
       <button onClick={Fangba}>Update Result</button>
       <p>Result: {result}</p>
     </div>
